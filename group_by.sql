5d2ec1ac9017156cfe9b918e213c0b5a662b0bab
Shar-03 <121991269+Shar-03@users.noreply.github.com>
2025-07-09 14:42:17 +0530

    Updated group_by.sql

--- group_by.sql
+++ group_by.sql
@@ -1,1 +1,9 @@
+-- Number of students per course
+SELECT course_id, COUNT(*) as student_count
+FROM students
+GROUP BY course_id;
 
+-- Average marks per subject
+SELECT subject_id, AVG(marks)
+FROM results
+GROUP BY subject_id;
