6a40707e4ceb74dca7c3615018dc231515a12142
Shar-03 <121991269+Shar-03@users.noreply.github.com>
2025-07-09 14:39:59 +0530

    Updated joins.sql

--- joins.sql
+++ joins.sql
@@ -1,1 +1,9 @@
+-- Inner join between students and courses
+SELECT s.name, c.course_name
+FROM students s
+JOIN courses c ON s.course_id = c.id;
 
+-- Left Join: list all students, even without a course
+SELECT s.name, c.course_name
+FROM students s
+LEFT JOIN courses c ON s.course_id = c.id;
