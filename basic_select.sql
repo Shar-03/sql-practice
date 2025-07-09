d32c1b391c53863199940b29fadcc055af0a10a7
Shar-03 <121991269+Shar-03@users.noreply.github.com>
2025-07-09 14:37:35 +0530

    Updated basic_select.sql

--- basic_select.sql
+++ basic_select.sql
@@ -1,1 +1,8 @@
+-- Fetch all students
+SELECT * FROM students;
 
+-- Get names and emails
+SELECT name, email FROM students;
+
+-- Get students born after 2000
+SELECT * FROM students WHERE dob > '2000-01-01';
